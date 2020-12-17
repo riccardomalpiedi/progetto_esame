10ad7ac33a47cbd26d969dfb42641c3e4f8045d0
riccardomalpiedi <S1092879@studenti.univpm.it>
2020-12-17 14:09:59 +0100

    prima modifica

--- WeatherApp/src/main/java/com/esame/WeatherApp/WeatherApplication.java
+++ WeatherApp/src/main/java/com/esame/WeatherApp/WeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherAppApplication {
+public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherAppApplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args);
 	}
 
 }
