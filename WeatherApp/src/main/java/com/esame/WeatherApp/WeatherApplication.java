b521f5472ee203b7460509d9951462268e5fc120
RiccardoVico <s1094005@studenti.univpm.it>
2020-12-17 15:23:09 +0100

    First Commit

--- WeatherApp/src/main/java/com/esame/WeatherApp/WeatherApplication.java
+++ WeatherApp/src/main/java/com/esame/WeatherApp/WeatherApplication.java
@@ -3,6 +3,12 @@ package com.esame.WeatherApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Riccardo Vico
+ *
+ */
+
 @SpringBootApplication
 public class WeatherApplication {
 
